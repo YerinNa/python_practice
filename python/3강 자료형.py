f6f184c88200dcd746d7832adde0ef801c4b644e
YerinNa <96712341+YerinNa@users.noreply.github.com>
2021-12-27 15:50:08 +0900

    Add files via upload

---
+++ python/3강 자료형.py
@@ -0,0 +1,117 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+안녕하세요.
+필요한 코드
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+A
+B
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+hi hello
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+이름을 입력해주세요.lululu
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+이름을 입력해주세요.lululu
+name
+'lululu'
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+이름을 입력해주세요.aaa
+안녕하세요 aaa
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+이름을 입력해주세요.
+aaa
+안녕하세요 aaa
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+이름을 입력해주세요.aaa
+안녕하세요 aaa
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+이름을 입력해주세요.bbbaaa
+안녕하세요 aaa
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+1
+2
+12
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+1
+2
+3
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+15
+5
+50
+2.0
+2
+0
+100000
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+7.0
+3.0
+10.0
+2.5
+2.0
+1.0
+25.0
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+True
+False
+
+True
+False
+
+True
+False
+True
+
+True
+False
+
+True
+False
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+12
+
+77
+777777
+
+안녕하세
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+숫자를 입력하세요.9
+99
+a
+'9'
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+숫자를 입력하세요.9
+18
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+Traceback (most recent call last):
+  File "C:/Users/나예린/Desktop/python/test1.py", line 71, in <module>
+    range(num)
+TypeError: 'float' object cannot be interpreted as an integer
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+
+================= RESTART: C:/Users/나예린/Desktop/python/test1.py ================
+8
+<class 'str'>
+<class 'int'>
+<class 'float'>
---
+++ python/test1.py
@@ -0,0 +1,81 @@
+#print('hi','hello')
+'''
+print('이름을 입력해주세요.')
+name=input()
+print('안녕하세요',name)
+'''
+#두 정수를 더하는 문제
+'''
+a=int(input())
+b=int(input())
+print(a+b)
+'''
+#자료형-숫자형
+'''
+a=10
+b=5
+c=2.0
+d=0.5
+
+print(b+c)
+print(b-c)
+print(b*c)
+print(b/c)
+print(b//c)
+print(b%c)
+print(b**c)
+
+'''
+#자료형-논리형
+'''
+x=10
+y=-10
+
+print(x>0)
+print(y>0)
+print()
+print(x>y)
+print(x<y)
+print()
+print(x==10)
+print(x==y)
+print(x!=y)
+print()
+print(x>0 or y>0)
+print(x>0 and y>0)
+print()
+print(x>0)
+print(not x>0)
+'''
+'''
+#문자열형
+a=6
+b="7"
+c=3.0
+
+print(a+a)
+print()
+print(b+b)
+print(a*b)
+print()
+print("안녕하세")
+'''
+
+'''
+#input()으로 숫자 입력 받기
+a=int(input('숫자를 입력하세요.'))
+print(a+a)
+'''
+'''
+#실수 정수
+num=5.0
+range(int(num))
+'''
+
+a=input()
+b=int(a)
+c=float(a)
+
+print(type(a))
+print(type(b))
+print(type(c))
